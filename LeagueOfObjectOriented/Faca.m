7c90d5aedee102ebf17b8c812b8e1125a585748a
Carlos-cass <beto.cass@yahoo.com>
2013-12-11 09:41:22 -0200

    Revert 02b8ba7..eb871d8
    This rolls back to commit 02b8ba75d5198c9ac30534871bb0650085cc95ba.

---
+++ LeagueOfObjectOriented/Faca.m
@@ -0,0 +1,42 @@
+//
+//  Faca.m
+//  LeagueOfObjectOriented
+//
+//  Created by GUSTAVO LUIS on 12/11/13.
+//  Copyright (c) 2013 Vinicius Miana. All rights reserved.
+//
+
+#import "Faca.h"
+#import "Jogador.h"
+
+@implementation Faca
+
+-(id)init{
+    self = [super init];
+    if(self){
+        desgaste = 0;
+        forca = 50;
+    }
+    return self;
+}
+
+@synthesize desgaste,forca;
+
+-(double) calculaForcaJogador:(Jogador *)novoJogador{
+    if(desgaste < 50) desgaste++;
+    if([novoJogador raca] == [Jogador ORC]){
+        return (([novoJogador forcaAtaque]+forca-desgaste)+7)*precisaoDoAtaque;
+    }
+    else if([novoJogador raca] == [Jogador HUMANO]){
+        return (([novoJogador forcaAtaque]+forca-desgaste)+12)*precisaoDoAtaque;
+    }
+    else if([novoJogador raca] == [Jogador ELFO]){
+        return (([novoJogador forcaAtaque]+forca-desgaste))*precisaoDoAtaque;
+    }
+    else if([novoJogador raca] == [Jogador ANAO]){
+        return (([novoJogador forcaAtaque]+forca-desgaste)+2)*precisaoDoAtaque;
+    }
+    return 0;
+}
+
+@end
